ae4d54a17391f3b108301e5f4a2dde0327895e79
Soto <sebastiansoto0298@gmail.com>
2026-02-22 20:52:42 -0600

    Create 22-02-2026-2.py

---
+++ Unidad2/22-02-2026-2.py
@@ -0,0 +1,205 @@
+class Libro:
+  def __init__(self, isbn, titulo, autor, categoria, precio):
+    self.isbn = isbn
+    self.titulo = titulo
+    self.autor = autor
+    self.categoria = categoria
+    self.precio = precio
+    self.prestado = False
+  def mostrar_info(self):
+      estado = "Prestado" if self.prestado else "Disponible"
+      print(f"[{self.isbn}] {self.titulo} - {self.autor} | {self.categoria} | ${self.precio} | {estado}")
+
+class Estudiante:
+  def __init__(self, num_control, nombre, carrera, fecha_nacimiento, telefono, correo):
+      self.num_control = num_control
+      self.nombre = nombre
+      self.carrera = carrera
+      self.fecha_nacimiento = fecha_nacimiento
+      self.telefono = telefono
+      self.correo = correo
+  def mostrar_info(self):
+      print(f"[{self.num_control}] {self.nombre} - {self.carrera} | Nac: {self.fecha_nacimiento} | Tel: {self.telefono} | Correo: {self.correo}")
+
+class Prestamo:
+  contador_prestamos = 1
+  def __init__(self, num_control, isbn, fecha_inicio, fecha_termino):
+        self.folio = Prestamo.contador_prestamos
+        Prestamo.contador_prestamos += 1
+        self.num_control = num_control
+        self.isbn = isbn
+        self.fecha_inicio = fecha_inicio
+        self.fecha_termino = fecha_termino
+
+        def mostrar_info(self):
+          print(f"Folio: {self.folio} | Estudiante: {self.num_control} | Libro: {self.libro} \
+          Del {self.fecha_inicio} al {self.fecha_termino}")
+
+class Venta:
+  contador_ventas = 1
+  def __init__(self, isbn, num_control, fecha, cantidad, importe):
+    self.folio_ventas = Venta.contador_ventas
+    Venta.contador_ventas += 1
+    self.isbn = isbn
+    self.num_control = num_control
+    self.fecha = fecha
+    self.cantidad = cantidad
+    self.importe = importe
+
+    def mostrar_info(self):
+      print(f"Folio: {self.folio_ventas} | ISBN: {self.isbn} | Comprador: {self.num_control} | \
+        Fecha: {self.fecha} | Cantidad: {self.cantidad} | Importe{self.importe}")
+
+
+     
+
+def alta_libro():
+    print("\n--- ALTA DE LIBRO ---")
+    isbn = input("ISBN: ")
+    for libro in libros:
+      if libro.isbn == isbn:
+        print("El libro ya existe con ese ISBN")
+        return
+    titulo = input("Titulo: ")
+    autor = input("Autor: ")
+    categoria = input("Categoria: ")
+    try:
+        precio = float(input("Precio: "))
+    except ValueError:
+      print("Precio Invalido")
+      return
+    nuevo_libro = Libro(isbn, titulo, autor, categoria, precio)
+    libros.append(nuevo_libro)
+    print("Libro dado de alta correctamente")
+
+     
+
+def alta_estudiante():
+  print("\n -- Alta de estudiante ---")
+  num_control = input("Numero de control: ")
+  for est in estudiantes:
+    if est.num_control == num_control:
+      print("El estudiante ya existe con ese numero de control")
+      return
+  nombre = input("Nombre: ")
+  carrera = input("Carrera: ")
+  fecha_nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): ")
+  telefono = input("Telefono: ")
+  correo = input("Correo: ")
+  nuevo_estudiante = Estudiante(num_control, nombre, carrera, fecha_nacimiento, telefono, correo)
+  estudiantes.append(nuevo_estudiante)
+  print("Estudiante dado de alta correctamente")
+    
+def consultar_libro():
+    print("\n--- CONSULTA DE LIBRO POR ISBN ---")
+    isbn = input("Ingrese el ISBN del libro: ")
+    for libro in libros:
+        if libro.isbn == isbn:
+             libro.mostrar_info()
+             return
+    print("Libro no encontrado")  
+
+def consultar_estudiante():
+    print("\n--- CONSULTA DE ESTUDIANTE POR NUMERO DE CONTROL ---")
+    num_control = input("Ingrese el numero de control del estudiante: ")
+    for est in estudiantes:
+        if est.num_control == num_control:
+             est.mostrar_info()
+             return
+    print("Estudiante no encontrado")
+
+def mostrar_todos_libros():
+    print("\n--- LISTADO DE TODOS LOS LIBROS ---")
+    if not libros:
+        print("No hay libros registrados")
+    else:
+        for libro in libros:
+            libro.mostrar_info()
+
+
+def mostrar_todos_estudiantes():
+    print("\n--- LISTADO DE TODOS LOS ESTUDIANTES ---")
+    if not estudiantes:
+        print("No hay estudiantes registrados")
+    else:
+        for est in estudiantes:
+            est.mostrar_info()
+
+def realizar_prestamo():
+    print("\n--- REALIZAR PRESTAMO ---")
+    num_control = input("Numero de control del estudiante: ")
+    isnt = input("ISBN del libro a prestar: ")
+    estudiante = next((e for e in estudiantes if e.num_control == num_control), None)
+    if not estudiante:
+        print("Estudiante no encontrado")
+        return
+    libro = next ((l for l in libros if i.isbn == isbn), None)
+    if not libro:
+        print("Libro no encontrado")
+        return
+    if libro.prestado:
+        print("El libro ya esta prestado")
+        return
+    fecha_inicio = input("Fecha de inicio del prestamo (dd/mm/aaaa): ")
+    fecha_termino = input("Fecha de termino del prestamo (dd/mm/aaaa): ")
+    prestamo = Prestamo(num_control, isbn, fecha_inicio, fecha_termino)
+    prestamos.append(prestamo)
+    libro.prestado = True
+    print(f"Prestamo registrado con folio {prestamo.folio}.")
+
+
+    def mostrar_prestamos():
+        print("\n--- LISTADO DE PRESTAMOS ---")
+        if not prestamos:
+            print("No hay prestamos registrados")
+        else:
+            for p in prestamos:
+                p.mostrar_info()
+
+def mostrar_menu():
+  print("\n--- SISTEMA DE LIBRERIA ESCOLAR ---")
+  print("1. Dar de alta un Libro")
+  print("2. Dar de alta estudiante")
+  print("3. Colsultar libro por ISBN")
+  print("4. Consultar estudiante por numero de control")
+  print("5. Mostrar todos los libros")
+  print("6. Mostrar todos los estudiantes")
+  print("7. Realizar prestamo")
+  print("8. Mostrar prestamos")
+  print("9. Devolver libro")
+  print("10. Registrar ventas")
+  print("11. Mostrar historial de Ventas")
+  print("12. Salir del sistema")
+
+def iniciar_sistema():
+  while True:
+      mostrar_menu()
+      opcion = input("Seleccione una opcion: ")
+      if opcion == 1:
+        alta_libro()
+      elif opcion == 2:
+        alta_estudiante()
+      elif opcion == 3:
+        consultar_libro()
+      elif opcion == 4:
+        consultar_estudiante()
+      elif opcion == 5:
+          mostrar_todos_libros()
+      elif opcion == 6:
+        mostrar_todos_estudiantes()
+      elif opcion == 7:
+          realizar_prestamo()
+      elif opcion == 8:
+          mostrar_prestamos()
+      elif opcion == 9:
+          devolver_libro()
+      elif opcion == 10:
+          registrar_venta()
+      elif opcion == 11:
+          mostrar_historial_ventas()
+      elif opcion == 12:
+          print("Saliendo del sistema...")
+          break
+      else:
+        print("Opcion invalida. Intente de nuevo")
+ 
